443a2f862e20d36faa8780604e4605deb41602f4
kurahashi@reas.jp <kurahashi@reas.jp>
2019-03-15 14:50:22 +0900

    MySQLデータの永続化するためボリュームを作成しMySQKコンテナにマウント

--- mysql/bootstrap/web.sql
+++ mysql/bootstrap/web.sql
@@ -1,13 +1,2 @@
 CREATE DATABASE IF NOT EXISTS web;
 use web;
-
-CREATE TABLE `operation_reward_target_point_log` (
-  `performer_code` int(10) unsigned NOT NULL,
-  `point` int(11) NOT NULL,
-  `key` varchar(255) NOT NULL,
-  `operation` enum('add','subtraction') NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  KEY `operation_reward_target_point_log_performer_code_index` (`performer_code`),
-  KEY `operation_reward_target_point_log_created_at_index` (`created_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
